bfe58c2babf8f0bbb1a3f6c0a58e72e7045d324a
SOVJ <simon.octavio.valdez@gmail.com>
2017-05-02 11:31:33 -0700

    Changed Column names, Added a My.Rank Column
    "My.Rank" column added to the user generated data table. 
    Reverted to ascending, because column 4"EDGE.Rank" is best listed in an ascending order.

--- server.R
+++ server.R
@@ -94,7 +94,7 @@ function(input, output) {
   ###Create selectable Original ED file from my ebird 
   output$origTable <- DT::renderDataTable({
     datatable(
-    ED, options = list(order = list(list(4, "des")), 
+    ED,colnames=c("Scientific.Name","Common.Name","EDGE.SCORE","EDGE.Rank","Hidden key for outdated names") options = list(order = list(list(4, "asc")), 
                       columnDefs = list(list(visible = FALSE, targets = c(0,5)))),
       selection = list(mode = "multiple")
     )
@@ -108,13 +108,16 @@ function(input, output) {
   ###Render a new table with selected species
   output$origTableSelected <- DT::renderDataTable({
     datatable(
-      origTable_selected(),
+      origTable_selected(),colnames=c("My.Rank","Scientific.Name","Common.Name","EDGE.Score","EDGE.Rank","Hidden key for outdated names"),
       options = list(dom = "t",order = list(list(4, "asc")),
                    scrollY = '250px', paging = FALSE ,
-                   columnDefs = list(list(visible = FALSE, targets = c(0,5)))),
+                   columnDefs = list(list(visible = FALSE, targets = c(5)))),
       selection = list(mode = "multiple"),
-      caption = "Your EDGE species"
-    )
+      caption = "Your EDGE species",
+      callback=JS("table.on( 'order.dt search.dt', function () {
+                                table.column(0, {search:'applied', order:'applied'}).nodes().each( function (cell, i) {
+                                                cell.innerHTML = i+1;});}).draw();"))
+  
   })
   
   ##Save the new table as csv.
